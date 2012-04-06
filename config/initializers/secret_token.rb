c41eea4524489f4a4a62ff686110ff65ba2d5456
Martin Provencher <mprovencher86@gmail.com>
2012-04-06 18:29:32 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MontrealrbContest::Application.config.secret_token = '06125f47f9a2200b7dbe99a2c2565936c309da4a7c76534588c882220cccd595c66246a9e700200c821651e48faa8d8a00f2c4d58fccae750126d51afd7fe0e3'
