e8a8b8b4187b69938e07eb766050f72ab082a292
Martin Provencher <mprovencher86@gmail.com>
2012-04-06 22:40:33 -0400

    General fixes

--- app/models/member.rb
+++ app/models/member.rb
@@ -2,6 +2,6 @@ class Member < ActiveRecord::Base
   attr_accessible :email, :name
 
   def gravatar_image
-    "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(@email)}?s=50"
+    "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(email)}?s=50"
   end
 end
