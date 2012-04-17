0a386dbbbd1ec3799a0e20f48bbe716613bcfe64
Martin Provencher <mprovencher86@gmail.com>
2012-04-17 12:40:16 -0400

    Fix routes for prizes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 MontrealrbContest::Application.routes.draw do
   devise_for :users, :sign_out_via => [:delete, :get]
 
-  resources :prizes, :only => [:index]
+  resources :prizes
   resources :members, :only => [:index, :new, :create]
   resources :images, :only => [:show]
 
