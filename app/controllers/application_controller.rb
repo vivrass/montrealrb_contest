019469cd44b55abc1a9e05483464a49f83c3ba74
Martin Provencher <mprovencher86@gmail.com>
2012-04-17 10:29:18 -0400

    Control which template_handler to use per subdomain

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
+  SUPPORTED_FORMAT = [:erb, :haml, :slim, :mustache]
   protect_from_forgery
+
+  before_filter :change_format
+
+  # Controller the available template handlers
+  def change_format
+    if current_subdomain && SUPPORTED_FORMAT.include?(current_subdomain.to_sym)
+      @template_handler = current_subdomain.to_sym
+    else
+      @template_handler = :erb
+    end
+    self.lookup_context.handlers = self.lookup_context.handlers - SUPPORTED_FORMAT + [@template_handler]
+  end
 end
